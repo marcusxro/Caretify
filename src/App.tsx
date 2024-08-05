d95b8c77e8411f9df5b469b4977d8e64444463a0
marcusxro <marcussalopaso1@gmail.com>
2024-08-05 12:35:58 +0800

    Fix TypeScript error and update build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import './App.css'
-import Header from './comps/Header'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 
-
 function App() {
-
-
   return (
     <Router>
       <div className="App">
