273eaf7daebf30b21c3c61b4a6cafea34f32101c
marcusxro <marcussalopaso1@gmail.com>
2024-08-06 11:18:59 +0800

    updated to v2

--- src/pages/SearchedItems.tsx
+++ src/pages/SearchedItems.tsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const SearchedItems: React.FC = () => {
-
     const location = useLocation();
-    const [results, setResults] = useState([]);
+    const [results, setResults] = useState<any>({ items: [] });
 
     useEffect(() => {
         const query = new URLSearchParams(location.search).get('q');
@@ -16,16 +15,25 @@ const SearchedItems: React.FC = () => {
         }
     }, [location.search]);
 
-
     useEffect(() => {
-        console.log(results)
-    }, [results])
+        console.log(results);
+    }, [results]);
+
+
+
 
     return (
-        <div>
-            asdasdasdasd
+        <div className='flex gap-5 flex-col text-center'>
+            {results.items?.map((itm: any) => (
+                <div
+                onClick={() => window.open(itm.formattedUrl, '_blank')}
+
+                 key={itm.cacheId}>
+                    {itm.title}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default SearchedItems
+export default SearchedItems;
